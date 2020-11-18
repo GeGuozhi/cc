498ac718f96bf7b5079b4d0b6165266887e0490e
wujunyi <junyi.wu@joyreach.com>
2020-11-18 19:25:05 +0800

    todo: 解析命令

--- server/cc-message/src/main/java/com/icu/cc/message/handler/CCInitialHandler.java
+++ server/cc-message/src/main/java/com/icu/cc/message/handler/CCInitialHandler.java
@@ -1,7 +1,10 @@
 package com.icu.cc.message.handler;
 
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.string.StringDecoder;
+import io.netty.handler.codec.string.StringEncoder;
 
 /**
  * Created by yi on 2020/11/17 23:11
@@ -10,7 +13,10 @@ public class CCInitialHandler extends ChannelInitializer<SocketChannel> {
 
     @Override
     protected void initChannel(SocketChannel ch) {
-        // todo
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast("encoder", new StringEncoder());
+        pipeline.addLast("decoder", new StringDecoder());
+        pipeline.addLast(new MessageHandler());
     }
 
 }
